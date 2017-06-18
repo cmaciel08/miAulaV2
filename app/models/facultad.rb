4f8bbf9431ac1ae09a9dbe339e192be85e9f65d7
vieyrapy <latinotanato@gmail.com>
2017-06-17 23:41:58 -0400

    Cabezera de login

--- app/models/facultad.rb
+++ app/models/facultad.rb
@@ -1,3 +1,4 @@
 class Facultad < ApplicationRecord
 	validates :nombre, presence:  true, length: {minimum: 5, maximum: 50, :message => ": Debe cargar cómo mínimo 5 caracteres y como máximo 50"}
+	has_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170618021254) do
+ActiveRecord::Schema.define(version: 20170618032330) do
 
   create_table "asignacion_aulas", force: :cascade do |t|
     t.string "observacion"
