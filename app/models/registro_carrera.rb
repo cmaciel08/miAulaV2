50eb2c70ad04a669429e15ee0b13c4d4da9c0668
Cristian Maciel <davidmacielmeza@gmail.com>
2017-07-06 15:24:14 -0400

    modelo

--- app/models/registro_carrera.rb
+++ app/models/registro_carrera.rb
@@ -1,6 +1,6 @@
 class RegistroCarrera < ApplicationRecord
 	validates :nombreCarrera, presence: true, length: {minimum: 5, maximum: 50, :message => ": Debe cargar cómo mínimo 5 caracteres y como máximo 50"}
-	validates :anho, presence: true, length: {minimum: 2, maximum: 10, :message => ": Debe cargar cómo mínimo 2 valor numérico y como máximo 10"}
+	validates :anho, presence: true, length: {minimum: 1, maximum: 10, :message => ": Debe cargar cómo mínimo 1 valor numérico y como máximo 10"}
 	validates :semestre, presence: true, length: {minimum: 2, maximum: 10, :message => ": Debe cargar cómo mínimo 2 valor numérico y como máximo 10"}
 	has_many :asignacion_aulas
 	has_many :usuarios
