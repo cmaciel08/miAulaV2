140aac78bfe3222761d6a439ba78dbf99807f37f
Cristian Maciel <davidmacielmeza@gmail.com>
2017-07-13 11:04:03 -0400

    modelo de asigbacion aula, observaciones, registo aula y semestre

--- app/models/asignacion_aula.rb
+++ app/models/asignacion_aula.rb
@@ -1,7 +1,8 @@
 class AsignacionAula < ApplicationRecord
-	validates :observacion, presence: true, length: {minimum: 5, maximum: 50, :message => ": Debe cargar cómo mínimo 5 caracteres y como máximo 50"}
 	belongs_to :anho_carrera
 	belongs_to :facultad
 	belongs_to :aula
 	belongs_to :registro_carrera
+	belongs_to :semestre
+	belongs_to :observacion
 end
