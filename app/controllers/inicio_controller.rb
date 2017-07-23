7b6229517e5c5750b0cd749c158a444ab534f41a
vieyrapy <latinotanato@gmail.com>
2017-07-23 16:18:08 -0400

    Controller inicio

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,23 +1,34 @@
 class InicioController < ApplicationController
-  #before_action :authenticate_user! 
+  before_action :authenticate_user! 
   # GET /asignacion_aulas
   # GET /asignacion_aulas.json
   def index
 
-    	 #@asignacion_aulas = AsignacionAula.all
- 		   #@asignacion_aulas = AsignacionAula.where(user_id: current_user.id)
- 		   #@asignacion_aulas = AsignacionAula.includes(:aula).where(aulas: {id: ID})
+    	#@asignacion_aulas = AsignacionAula.all
+ 		  #@asignacion_aulas = AsignacionAula.where(user_id: current_user.id)
+ 		  #@asignacion_aulas = AsignacionAula.includes(:aula).where(aulas: {id: ID})
   		#@asignacion_aulas = AsignacionAula.where(anho_carrera_id: 1 )
   		#@asignacion_aulas = AsignacionAula.where(current_user.registro_carrera.asignacion_aulas)
     	#@asignacion_aulas = AsignacionAula.where(["users.registro_carrera_id=registro_carreras.id"])
     	#@productos = Producto.where(["nombre LIKE ?","%#{params[:Buscar]}%"]).order(:precio)
-    	if current_user
-			   @asignacion_aulas = current_user.facultad.asignacion_aulas
-      elsif current_admin
+    	#if current_user
+			   #@asignacion_aulas = current_user.facultad.asignacion_aulas
+      #elsif current_admin
+         #@asignacion_aulas = AsignacionAula.all
+		  #else
+			   #@asignacion_aulas = AsignacionAula.all
+		  #end
+
+      if !current_user.facultad_id 
+         #@asignacion_aulas = AsignacionAula.all
+         #Redireccionar a editar 
+         redirect_to edit_user_registration_path
+         #@asignacion_aulas = current_user.facultad.asignacion_aulas
+      elsif current_user
+        @asignacion_aulas = current_user.facultad.asignacion_aulas
+      else
          @asignacion_aulas = AsignacionAula.all
-		  else
-			   @asignacion_aulas = AsignacionAula.all
-		end
+      end
 	end
   # GET /asignacion_aulas/
   def show
