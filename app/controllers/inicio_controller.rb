61449e3955b0f46ed28eba24fd3a215ca4ce14b4
vieyrapy <latinotanato@gmail.com>
2017-07-22 23:15:40 -0400

    una mas

--- app/controllers/inicio_controller.rb
+++ app/controllers/inicio_controller.rb
@@ -1,5 +1,5 @@
 class InicioController < ApplicationController
-  #before_action :authenticate_user! 
+  before_action :authenticate_user! 
   # GET /asignacion_aulas
   # GET /asignacion_aulas.json
   def index
